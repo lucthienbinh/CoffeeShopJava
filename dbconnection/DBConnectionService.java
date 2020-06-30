6b66e6b4ab64e524a250f905c089a4b2e72b8a22
lucthienbinh <lucthienbinh9a6ductri@gmail.com>
2020-06-30 15:21:52 +0700

    add DBConnectionService.java to remote repo

---
+++ dbconnection/DBConnectionService.java
@@ -0,0 +1,47 @@
+
+package coffeeshop.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnectionService {
+
+    protected static void loadJDBCDriver() throws Exception {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (java.lang.ClassNotFoundException e) {
+            throw new Exception("SQL JDBC Driver not found ...");
+        }
+    }
+
+    public static Connection getConnection() throws Exception {
+	    Connection connect = null;
+	    if (connect == null) {
+	    	loadJDBCDriver();
+	    try {
+	    
+	    	connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/caphe_java_db?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+	    			"root", "");
+	    } catch (java.sql.SQLException e) {
+	    	throw new Exception("Can not access to Database Server ..." + e.getMessage());
+	    	}
+	    }
+	    return connect;
+    }
+    
+    
+//    public static Connection getConnection() throws Exception {
+//        Connection connect = null;
+//        if (connect == null) {
+//            try {                
+//                DataSource ds = (DataSource) new InitialContext().
+//                        lookup("jdbc/j2ee_MySQLConnection");                
+//                return ds.getConnection();
+//            } catch (Exception e) {
+//                throw new Exception("Can not access to Database Server ..." + e.getMessage());
+//            }
+//        }
+//        return connect;
+//    }
+    
+}
